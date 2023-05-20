8a77fa88027665f77dd5a2dec330b65e1515a292
Sohel Rana <expartdesigns5@gmail.com>
2023-05-20 21:07:47 +0600

    delete

--- src/components/Qty.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Qty = () => {
-  return <div>Qty</div>;
-};
-
-export default Qty;
--- src/request.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export const request = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
-  headers: {
-    Authorization: 'bearer ' + process.env.REACT_APP_API_KEY,
-  },
-});
