38c113531e8c691c73b1340f9afc3489cb118fe1
Sohel Rana <expartdesigns5@gmail.com>
2023-05-21 12:35:20 +0600

    Headertop Responsibe

--- src/components/Headertop.js
+++ src/components/Headertop.js
@@ -5,8 +5,8 @@ import { Link } from "react-router-dom";
 
 const Headertop = () => {
     return (
-        <section className="bg-primary">
-            <div className="container mx-auto py-1 flex justify-between">
+        <section className="bg-primary hidden sm:block">
+            <div className="container mx-auto py-1 flex justify-between md:px-3">
                 <div className="flex gap-x-3">
                     <div className="flex  items-center gap-x-2 ">
                         <BsFillTelephoneFill className="text-white font-normal text-xs font-open" />
