bd8d2156f1afc4bcaa8eed56f107d117e52e72a7
Sohel Rana <expartdesigns5@gmail.com>
2023-05-21 11:57:08 +0600

    headertop link

--- src/components/Headertop.js
+++ src/components/Headertop.js
@@ -1,14 +1,36 @@
-import React from 'react'
+import React from "react";
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 const Headertop = () => {
-  return (
-    <section className='bg-primary'>
-        <div className='container mx-auto py-3 flex'>
-            <div></div>
-            <div></div>
-        </div>
-    </section>
-  )
-}
+    return (
+        <section className="bg-primary">
+            <div className="container mx-auto py-1 flex justify-between">
+                <div className="flex gap-x-3">
+                    <div className="flex  items-center gap-x-2 ">
+                        <BsFillTelephoneFill className="text-white font-normal text-xs font-open" />
+                        <span className="text-white font-normal text-xs font-open">
+                            +88012 3456 7894
+                        </span>
+                    </div>
+                    <div className="flex items-center gap-x-2">
+                        <BsFillTelephoneFill className="text-white font-normal text-xs font-open" />
+                        <span className="text-white font-normal text-xs font-open">
+                            +88012 3456 7894
+                        </span>
+                    </div>
+                </div>
 
-export default Headertop
+                <div className="flex">
+                    <ul>
+                        <Link to='#'>
+                            <li className="text-white">Theme FAQ"s</li>
+                        </Link>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Headertop;
